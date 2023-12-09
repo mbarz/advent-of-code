c910c0f032f8eb5f1d8cfe20237fe442a461121c
muxe <mbarz@muxe.de>
2023-12-09 21:59:22 +0100

    2023 - day 09 - refactoring

--- src/2023/2023-09-mirage-maintenance.ts
+++ src/2023/2023-09-mirage-maintenance.ts
@@ -6,35 +6,26 @@ export class AdventOfCode2023Day09 extends Solver {
   constructor(sub?: string) {
     super(9, sub);
   }
+
   solvePart1(): number {
-    const given = this.lines.map((line) => parseNumbers(line));
-    const next = given.map((seq) => this.findNextForSeq(seq));
-    return sum(next);
+    return sum(
+      this.lines.map((l) => parseNumbers(l)).map((s) => this.findNextForSeq(s))
+    );
   }
 
   findNextForSeq(seq: number[]): number {
-    const diffs = seq.slice(0, -1).map((n, i) => {
-      return seq[i + 1] - n;
-    });
+    const diffs = seq.slice(0, -1).map((n, i) => seq[i + 1] - n);
     const last = seq[seq.length - 1];
     const lastDiff = diffs[diffs.length - 1];
-    if (new Set(diffs).size === 1) return last + lastDiff;
-    return last + this.findNextForSeq(diffs);
+    const done = new Set(diffs).size === 1;
+    return last + (done ? lastDiff : this.findNextForSeq(diffs));
   }
 
   solvePart2(): number {
-    const given = this.lines.map((line) => parseNumbers(line));
-    const prev = given.map((seq) => this.findPrevForSeq(seq));
-    return sum(prev);
-  }
-
-  findPrevForSeq(seq: number[]): number {
-    const diffs = seq.slice(0, -1).map((n, i) => {
-      return seq[i + 1] - n;
-    });
-    const first = seq[0];
-    const firstDiff = diffs[0];
-    if (new Set(diffs).size === 1) return first - firstDiff;
-    return first - this.findPrevForSeq(diffs);
+    return sum(
+      this.lines
+        .map((l) => parseNumbers(l))
+        .map((s) => this.findNextForSeq(s.reverse()))
+    );
   }
 }
