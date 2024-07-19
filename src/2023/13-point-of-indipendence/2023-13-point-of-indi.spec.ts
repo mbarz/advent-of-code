92cd645979f74830faf05a8ecf01ed3e43ef60aa
Max-Richard Barz <max-richard.barz@siemens.com>
2024-07-19 10:42:21 +0200

    2023 - day 13

---
+++ src/2023/13-point-of-indipendence/2023-13-point-of-indi.spec.ts
@@ -0,0 +1,115 @@
+import { readFileSync } from 'fs';
+import {
+  AdventOfCode2023Day13,
+  isLineMirroredAt,
+  isPatternHorizontallyMirroredAt,
+  rotatePattern,
+} from './2023-13-point-of-indi';
+
+describe('2023 Day 13: Point of Incidence', () => {
+  const testInput = `
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#`;
+
+  const solver = new AdventOfCode2023Day13(testInput);
+
+  it('should parse patterns', () => {
+    const patterns = solver.parsePatterns(testInput);
+    expect(patterns).toHaveLength(2);
+    expect(patterns[0][0]).toEqual('#.##..##.');
+  });
+
+  it('should check mirror on line', () => {
+    expect(isLineMirroredAt('#.##..##', 1)).toEqual(false);
+    expect(isLineMirroredAt('#.##..##', 2)).toEqual(false);
+    expect(isLineMirroredAt('#.##..##', 5)).toEqual(true);
+    expect(isLineMirroredAt('#.##..##', 7)).toEqual(true);
+
+    expect(isLineMirroredAt('.#...##..', '.#...##..'.length - 1)).toEqual(true);
+
+    expect(isLineMirroredAt('##..########.', 3)).toEqual(true);
+  });
+
+  it('should check mirror on pattern', () => {
+    const patterns = solver.parsePatterns(testInput);
+    expect(isPatternHorizontallyMirroredAt(patterns[0], 3)).toEqual(false);
+    expect(isPatternHorizontallyMirroredAt(patterns[0], 5)).toEqual(true);
+  });
+
+  it('should solve part 1 for example', () => {
+    const result = solver.solvePart1();
+    expect(result).toEqual(405);
+  });
+
+  it('should toggle pixel', () => {
+    expect(solver.togglePixel(['##', '##'], 0)).toEqual(['.#', '##']);
+    expect(solver.togglePixel(['##', '##'], 1)).toEqual(['#.', '##']);
+    expect(solver.togglePixel(['##', '##'], 2)).toEqual(['##', '.#']);
+    expect(solver.togglePixel(['##', '##'], 3)).toEqual(['##', '#.']);
+  });
+
+  it('should get value for pattern', () => {
+    expect(
+      solver.getReflectionValues([
+        '..##..##.',
+        '..#.##.#.',
+        '##......#',
+        '##......#',
+        '..#.##.#.',
+        '..##..##.',
+        '#.#.##.#.',
+      ]),
+    ).toEqual([5, 300]);
+  });
+
+  it('should get value without smudge for first pattern in example', () => {
+    const value = solver.getRealReflectionValue(solver.patterns[0]);
+    expect(value).toEqual(300);
+  });
+
+  it('should get value without smudge for specific pattern', () => {
+    const pattern = rotatePattern([
+      '####..######.',
+      '....#.......#',
+      '.##.#........',
+      '####.########',
+      '#####.##..##.',
+      '....#.#....#.',
+      '#..#....##...',
+    ]);
+    const withSmudge = solver.getReflectionValue(pattern);
+    const value = solver.getRealReflectionValue(pattern);
+    expect(withSmudge).toEqual(200);
+    expect(value).toEqual(900);
+  });
+
+  it('should solve part 2 for example', () => {
+    const result = solver.solvePart2();
+    expect(result).toEqual(400);
+  });
+
+  it('should solve part 1 from puzzle', () => {
+    const fileContent = readFileSync(__dirname + '/input-13.txt', 'utf-8');
+    const result = new AdventOfCode2023Day13(fileContent).solvePart1();
+    expect(result).toEqual(37113);
+  });
+
+  it.skip('should solve part 2 from puzzle', () => {
+    const fileContent = readFileSync(__dirname + '/input-13.txt', 'utf-8');
+    const result = new AdventOfCode2023Day13(fileContent).solvePart2();
+    expect(result).toEqual(30449);
+  });
+});
---
+++ src/2023/13-point-of-indipendence/2023-13-point-of-indi.ts
@@ -0,0 +1,121 @@
+import { Solver } from '../util/solver';
+
+export class AdventOfCode2023Day13 implements Solver {
+  patterns: string[][];
+
+  constructor(input: string) {
+    this.patterns = this.parsePatterns(input);
+  }
+
+  parsePatterns(puzzle: string) {
+    const patterns: string[][] = [];
+    const lines = puzzle.trim().split('\n');
+    patterns.push([]);
+    for (const line of lines) {
+      const currentPattern = patterns[patterns.length - 1];
+      if (!line && currentPattern.length) patterns.push([]);
+      else currentPattern.push(line);
+    }
+    return patterns;
+  }
+
+  getReflectionValue(pattern: string[]) {
+    return findHorizontalMirror(pattern) ?? 100 * findVerticalMirror(pattern)!;
+  }
+
+  getReflectionValues(pattern: string[]): number[] {
+    return [
+      ...findHorizontalMirrors(pattern),
+      ...findVerticalMirrors(pattern).map((m) => 100 * m),
+    ].filter((n) => n != null && !isNaN(n));
+  }
+
+  togglePixel(pattern: string[], pixel: number) {
+    const copy = [...pattern];
+    const width = pattern[0].length;
+    2;
+    const y = Math.floor(pixel / width);
+    const x = pixel % width;
+    const current = copy[y][x];
+    const replacement = current === '.' ? '#' : '.';
+    copy[y] = copy[y].substring(0, x) + replacement + copy[y].substring(x + 1);
+    return copy;
+  }
+
+  getRealReflectionValue(pattern: string[]) {
+    const pixels = pattern.length * pattern[0].length;
+    const withSmudge = this.getReflectionValue(pattern);
+    for (let i = 0; i < pixels; ++i) {
+      const variant = this.togglePixel(pattern, i);
+      const newMirror = this.getReflectionValues(variant).find(
+        (v) => v != withSmudge,
+      );
+      if (newMirror) {
+        return newMirror;
+      }
+    }
+    throw new Error(
+      `Did not find new mirror for ${this.patterns.indexOf(pattern)}`,
+    );
+  }
+
+  solvePart1(): number {
+    return this.patterns.reduce(
+      (prev, curr) => prev + this.getReflectionValue(curr),
+      0,
+    );
+  }
+  solvePart2(): number {
+    return this.patterns.reduce(
+      (prev, curr) => prev + this.getRealReflectionValue(curr),
+      0,
+    );
+  }
+}
+
+export function isLineMirroredAt(line: string, index: number): boolean {
+  const a = line.substring(0, index);
+  const b = line.substring(index);
+  const bMirr = b.split('').reverse().join('');
+  return a.endsWith(bMirr) || bMirr.endsWith(a);
+}
+
+export function isPatternHorizontallyMirroredAt(
+  pattern: string[],
+  index: number,
+): boolean {
+  return pattern.find((l) => !isLineMirroredAt(l, index)) == null;
+}
+
+export function findHorizontalMirrors(pattern: string[]): number[] {
+  const mirros = [];
+  for (let i = 1; i < pattern[0].length; i++) {
+    if (isPatternHorizontallyMirroredAt(pattern, i)) mirros.push(i);
+  }
+  return mirros;
+}
+
+export function findHorizontalMirror(pattern: string[]): number | undefined {
+  for (let i = 1; i < pattern[0].length; i++) {
+    if (isPatternHorizontallyMirroredAt(pattern, i)) return i;
+  }
+  return undefined;
+}
+
+export function rotatePattern(pattern: string[]): string[] {
+  return Array(pattern[0].length)
+    .fill('')
+    .map((_a, x) =>
+      Array(pattern.length)
+        .fill(0)
+        .map((_b, y) => pattern[y][x])
+        .join(''),
+    );
+}
+
+export function findVerticalMirror(pattern: string[]): number | undefined {
+  return findHorizontalMirror(rotatePattern(pattern));
+}
+export function findVerticalMirrors(pattern: string[]): number[] {
+  return findHorizontalMirrors(rotatePattern(pattern));
+}
