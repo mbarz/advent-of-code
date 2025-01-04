787a26bcf5aaec9e10a46b2dbd4e94abc5779184
Max-Richard Barz <max-richard.barz@siemens.com>
2025-01-04 13:08:24 +0100

    Day 2024 21

--- src/2024/21/2421.spec.ts
+++ src/2024/21/2421.spec.ts
@@ -1,4 +1,12 @@
-import { getScore, getShortestInput, NAVPAD, NUMPAD, solvePart1 } from './2421';
+import {
+  getPossibleInputs,
+  getScore,
+  getShortestInput,
+  iterate,
+  NAVPAD,
+  NUMPAD,
+  solvePart1,
+} from './2421';
 
 const e1 = `029A
 980A
@@ -10,10 +18,10 @@ describe('2024 Day 21', () => {
   it('should get shortest num input', () => {
     expect(getShortestInput('029A', NUMPAD)).toEqual('<A^A>^^AvvvA');
     expect(getShortestInput('<A^A>^^AvvvA', NAVPAD)).toEqual(
-      '<<vA>>^A<A>AvA<^AA>A<vAAA>^A',
+      'v<<A>>^A<A>AvA<^AA>A<vAAA>^A',
     );
     expect(getShortestInput('<A^A>^^AvvvA', NAVPAD)).toEqual(
-      '<<vA>>^A<A>AvA<^AA>A<vAAA>^A',
+      'v<<A>>^A<A>AvA<^AA>A<vAAA>^A',
     );
   });
 
@@ -21,9 +29,14 @@ describe('2024 Day 21', () => {
     expect(getShortestInput('179A', NUMPAD)).toEqual('^<<A^^A>>AvvvA');
   });
 
+  it('should get possible inputs', () => {
+    expect(getPossibleInputs('>A', NAVPAD)).toEqual(['vA^A']);
+    expect(getPossibleInputs('>^A', NAVPAD)).toEqual(['vA^<A>A', 'vA<^A>A']);
+  });
+
   it('should get shortest inputs for directions', () => {
     expect(getShortestInput('>A', NAVPAD)).toEqual('vA^A');
-    expect(getShortestInput('<A', NAVPAD)).toEqual('<<vA>>^A');
+    expect(getShortestInput('<A', NAVPAD)).toEqual('v<<A>>^A');
     expect(getShortestInput('vA', NAVPAD)).toEqual('<vA>^A');
     expect(getShortestInput('^A', NAVPAD)).toEqual('<A>A');
 
@@ -45,4 +58,30 @@ describe('2024 Day 21', () => {
   it('should solve part 1', () => {
     expect(solvePart1(e1)).toEqual(126384);
   });
+
+  it('should solve more fast', () => {
+    expect(iterate('<', 1)).toEqual(4);
+    expect(iterate('<', 2)).toEqual(iterate('<<vA', 1));
+    expect(iterate('<', 3)).toEqual(iterate('v<<A', 2));
+
+    expect(iterate('<A', 1)).toEqual(8);
+    expect(iterate('<A', 2)).toEqual(iterate('v<<A', 1) + iterate('>>^A', 1));
+    expect(iterate('<A', 3)).toEqual(iterate('v<<A', 2) + iterate('>>^A', 2));
+
+    expect(iterate('v<<A')).toEqual(10);
+    expect(iterate('v<<A', 2)).toEqual(iterate('v<<A', 2));
+
+    expect(iterate('<A^A>^^AvvvA')).toEqual(28);
+    expect(iterate('<A^A>^^AvvvA', 2)).toEqual(68);
+
+    expect(getScore('029A', 1)).toEqual(348);
+    expect(getScore('029A', 2)).toEqual(812);
+    expect(getScore('029A', 3)).toEqual(1972);
+    expect(getScore('029A', 4)).toEqual(4756);
+    expect(getScore('029A', 5)).toEqual(11716);
+  });
+
+  // it('should solve part 1', () => {
+  //   // expect(solvePart2(e1)).toEqual(126384);
+  // });
 });
--- src/2024/21/2421.ts
+++ src/2024/21/2421.ts
@@ -27,42 +27,134 @@ export const NUMPAD = [
   [' ', '0', 'A'],
 ];
 
+const [U, D, L, R, E, A] = ['^', 'v', '<', '>', ' ', 'A'];
+
 export const NAVPAD = [
-  [' ', '^', 'A'],
-  ['<', 'v', '>'],
+  [E, U, A],
+  [L, D, R],
 ];
 
 export function getShortestInput(out: string, layout: string[][]): string {
   let instructions = '';
   const digits = out.split('');
-  let pos = findCoords('A', layout)!;
+  let pos = findCoords(A, layout)!;
   while (digits.length) {
     const target = digits.shift()!;
     const to = findCoords(target, layout)!;
     const dx = to.x - pos.x;
     const dy = to.y - pos.y;
-    const avoidBlank = layout[pos.y][pos.x + dx] === ' ';
-    if (avoidBlank && dy < 0) instructions += '^'.repeat(-dy);
-    if (dx < 0) instructions += '<'.repeat(-dx);
-    if (dx > 0) instructions += '>'.repeat(dx);
-    if (dy > 0) instructions += 'v'.repeat(dy);
-    if (!avoidBlank && dy < 0) instructions += '^'.repeat(-dy);
-    instructions += 'A';
+    const verticalFirst = layout[pos.y][pos.x + dx] === E;
+    const horizontalFirst = layout[pos.y + dy][pos.x] === E;
+    if (verticalFirst) {
+      instructions += (dy < 0 ? U : D).repeat(Math.abs(dy));
+      instructions += (dx < 0 ? L : R).repeat(Math.abs(dx));
+    } else if (horizontalFirst) {
+      instructions += (dx < 0 ? L : R).repeat(Math.abs(dx));
+      instructions += (dy < 0 ? U : D).repeat(Math.abs(dy));
+    } else {
+      instructions += (dx < 0 ? L : R).repeat(Math.abs(dx));
+      instructions += (dy < 0 ? U : D).repeat(Math.abs(dy));
+    }
+    instructions += A;
     pos = to;
   }
-
   return instructions;
 }
 
-export function getScore(n: string): number {
+export function getPossibleInputs(out: string, layout: string[][]): string[] {
+  let inputs = [''];
+  const digits = out.split('');
+  let pos = findCoords(A, layout)!;
+  while (digits.length) {
+    const target = digits.shift()!;
+    const to = findCoords(target, layout)!;
+    const dx = to.x - pos.x;
+    const dy = to.y - pos.y;
+    const verticalFirst = layout[pos.y][pos.x + dx] === E;
+    const horizontalFirst = layout[pos.y + dy][pos.x] === E;
+    if (verticalFirst || !dx || !dy) {
+      inputs.forEach((input, i) => {
+        input += (dy < 0 ? U : D).repeat(Math.abs(dy));
+        input += (dx < 0 ? L : R).repeat(Math.abs(dx));
+        inputs[i] = input;
+      });
+    } else if (horizontalFirst) {
+      inputs.forEach((input, i) => {
+        input += (dx < 0 ? L : R).repeat(Math.abs(dx));
+        input += (dy < 0 ? U : D).repeat(Math.abs(dy));
+        inputs[i] = input;
+      });
+    } else {
+      inputs = inputs.reduce((prev, input) => {
+        let a = input;
+        let b = input;
+        a += (dy < 0 ? U : D).repeat(Math.abs(dy));
+        a += (dx < 0 ? L : R).repeat(Math.abs(dx));
+        b += (dx < 0 ? L : R).repeat(Math.abs(dx));
+        b += (dy < 0 ? U : D).repeat(Math.abs(dy));
+        return [...prev, a, b];
+      }, [] as string[]);
+    }
+    inputs.forEach((input, i) => {
+      inputs[i] = input + A;
+    });
+
+    pos = to;
+  }
+  return inputs;
+}
+
+export function getScore(n: string, navpads = 3): number {
   const num = Number(n.substring(0, n.length - 1));
-  const a = getShortestInput(n, NUMPAD);
-  const b = getShortestInput(a, NAVPAD);
-  const c = getShortestInput(b, NAVPAD);
-  const l = c.length;
-  return l * num;
+  const ways = getPossibleInputs(n, NUMPAD);
+  let min = Infinity;
+  for (const way of ways) {
+    const l = iterate(way, navpads - 1);
+    if (l * num < min) min = l * num;
+  }
+  return min;
+}
+
+const cache: Map<string, number> = new Map();
+
+export function iterate(instructions: string, navpads = 1): number {
+  if (navpads <= 0) return instructions.length;
+  if (navpads === 1) {
+    const possible = getPossibleInputs(instructions, NAVPAD);
+    return Math.min(...possible.map((p) => p.length));
+  }
+
+  const key = [instructions, navpads].join(':');
+  if (cache.has(key)) {
+    return cache.get(key)!;
+  }
+
+  const variants = getPossibleInputs(instructions, NAVPAD);
+  let min = Infinity;
+  for (const variant of variants) {
+    const parts = variant.split('A').map((p) => p + 'A');
+    const res = parts
+      .slice(0, -1)
+      .map((part) => iterate(part, navpads - 1))
+      .reduce((acc, l) => acc + l, 0);
+    if (res < min) min = res;
+  }
+  const res = min;
+  cache.set(key, res);
+  return res;
+}
+
+export function solve(input: string, inputs: number): number {
+  cache.clear();
+  return input
+    .split('\n')
+    .reduce((acc, line) => acc + getScore(line, inputs), 0);
 }
 
 export function solvePart1(input: string): number {
-  return input.split('\n').reduce((acc, line) => acc + getScore(line), 0);
+  return solve(input, 3);
+}
+
+export function solvePart2(input: string): number {
+  return solve(input, 26);
 }
